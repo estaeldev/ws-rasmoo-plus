55261ee38a00856e78a4e2a5be790e257fc6593a
estaeldev <estael.dev@gmail.com>
2023-06-28 18:08:52 -0300

    conteudo desenvolvido em aula

---
+++ src/test/java/com/client/ws/rasmooplus/controller/UserTypeControllerTest.java
@@ -0,0 +1,58 @@
+package com.client.ws.rasmooplus.controller;
+
+import static org.mockito.Mockito.when;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import com.client.ws.rasmooplus.dto.UserTypeDto;
+import com.client.ws.rasmooplus.service.UserTypeService;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc(addFilters = false)
+public class UserTypeControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private UserTypeService userTypeService;
+
+    @Test
+    void findAll_then_returnAllUserType() throws Exception {
+        List<UserTypeDto> userTypeDtoList = new ArrayList<>();
+
+        UserTypeDto userType1 = UserTypeDto.builder()
+            .id(1L)
+            .name("Professor")
+            .description("Professor da plataforma")
+            .build();
+
+        UserTypeDto userType2 = UserTypeDto.builder()
+            .id(2L)
+            .name("Administrador")
+            .description("Administrador da plataforma")
+            .build();
+
+        userTypeDtoList.add(userType1);        
+        userTypeDtoList.add(userType2);
+
+        when(this.userTypeService.findAll()).thenReturn(userTypeDtoList);
+        
+        this.mockMvc.perform(MockMvcRequestBuilders.get("/user-type"))
+            .andExpect(MockMvcResultMatchers.status().isOk());
+
+    }
+
+
+
+}
--- src/test/java/com/client/ws/rasmooplus/integration/WsRaspayIntegrationTest.java
+++ src/test/java/com/client/ws/rasmooplus/integration/WsRaspayIntegrationTest.java
@@ -4,6 +4,7 @@ import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
+import java.math.BigDecimal;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -23,6 +24,8 @@ import org.springframework.test.util.ReflectionTestUtils;
 import org.springframework.web.client.RestTemplate;
 
 import com.client.ws.rasmooplus.dto.wsraspay.CustomerDto;
+import com.client.ws.rasmooplus.dto.wsraspay.OrderDto;
+import com.client.ws.rasmooplus.dto.wsraspay.PaymentDto;
 import com.client.ws.rasmooplus.exception.WsRaspayException;
 import com.client.ws.rasmooplus.integration.impl.WsRaspayIntegrationImpl;
 
@@ -108,6 +111,104 @@ class WsRaspayIntegrationTest {
             .exchange("http://localhost:8080/customer", HttpMethod.POST, request, CustomerDto.class);
 
     }
+
+    @Test
+    void createOrder_when_apiResponseIs201Created_then_returnOrderDto() {
+        ReflectionTestUtils.setField(wsRaspayIntegration, "raspayHost", "http://localhost:8080");
+        ReflectionTestUtils.setField(wsRaspayIntegration, "orderUrl", "/order");
+
+        OrderDto orderDto = OrderDto.builder()
+            .id(UUID.randomUUID().toString())
+            .discount(BigDecimal.valueOf(5L))
+            .build();
+
+        HttpEntity<OrderDto> request = new HttpEntity<>(orderDto, headers);
+
+        when(this.restTemplate.exchange("http://localhost:8080/order", 
+            HttpMethod.POST, request, OrderDto.class)).thenReturn(ResponseEntity.of(Optional.of(orderDto)));
+
+        OrderDto createOrder = this.wsRaspayIntegration.createOrder(orderDto);
+
+        Assertions.assertNotNull(createOrder);
+        Assertions.assertEquals(orderDto, createOrder);
+        Assertions.assertEquals(orderDto.getId(), createOrder.getId());
+        
+        verify(this.restTemplate, times(1))
+            .exchange("http://localhost:8080/order", HttpMethod.POST, request, OrderDto.class);
+
+    }
+
+    @Test
+    void createOrder_when_apiResponseGetThrows_then_returnWsRaspayException() {
+        ReflectionTestUtils.setField(wsRaspayIntegration, "raspayHost", "http://localhost:8080");
+        ReflectionTestUtils.setField(wsRaspayIntegration, "orderUrl", "/order");
+
+        OrderDto orderDto = OrderDto.builder()
+            .id(UUID.randomUUID().toString())
+            .discount(BigDecimal.valueOf(5L))
+            .build();
+
+        HttpEntity<OrderDto> request = new HttpEntity<>(orderDto, headers);
+
+        when(this.restTemplate.exchange("http://localhost:8080/order", 
+            HttpMethod.POST, request, OrderDto.class)).thenThrow(WsRaspayException.class);
+
+        Assertions.assertThrows(WsRaspayException.class,
+            () -> this.wsRaspayIntegration.createOrder(orderDto));
+        
+        verify(this.restTemplate, times(1))
+            .exchange("http://localhost:8080/order", HttpMethod.POST, request, OrderDto.class);
+
+    }
+
+    @Test
+    void processPayment_when_apiResponseIs201Created_then_returnTrue() {
+        ReflectionTestUtils.setField(wsRaspayIntegration, "raspayHost", "http://localhost:8080");
+        ReflectionTestUtils.setField(wsRaspayIntegration, "paymentUrl", "/payment");
+
+        PaymentDto paymentDto = PaymentDto.builder()
+            .customerId(UUID.randomUUID().toString())
+            .orderId(UUID.randomUUID().toString())
+            .build();        
+
+        HttpEntity<PaymentDto> request = new HttpEntity<>(paymentDto, headers);
+
+        when(this.restTemplate.exchange("http://localhost:8080/payment", 
+            HttpMethod.POST, request, Boolean.class)).thenReturn(ResponseEntity.of(Optional.of(Boolean.TRUE)));
+
+        Boolean processPayment = this.wsRaspayIntegration.processPayment(paymentDto);
+
+        Assertions.assertNotNull(processPayment);
+        Assertions.assertTrue(processPayment);
+        
+        verify(this.restTemplate, times(1))
+            .exchange("http://localhost:8080/payment", HttpMethod.POST, request, Boolean.class);
+
+    }
+    
+    @Test
+    void processPayment_when_apiResponseGetThrows_then_returnWsRaspayException() {
+        ReflectionTestUtils.setField(wsRaspayIntegration, "raspayHost", "http://localhost:8080");
+        ReflectionTestUtils.setField(wsRaspayIntegration, "paymentUrl", "/payment");
+
+        PaymentDto paymentDto = PaymentDto.builder()
+            .customerId(UUID.randomUUID().toString())
+            .orderId(UUID.randomUUID().toString())
+            .build();        
+
+        HttpEntity<PaymentDto> request = new HttpEntity<>(paymentDto, headers);
+
+        when(this.restTemplate.exchange("http://localhost:8080/payment", 
+            HttpMethod.POST, request, Boolean.class)).thenThrow(WsRaspayException.class);
+        
+        Assertions.assertThrows(WsRaspayException.class,
+            () -> this.wsRaspayIntegration.processPayment(paymentDto));
+        
+        verify(this.restTemplate, times(1))
+            .exchange("http://localhost:8080/payment", HttpMethod.POST, request, Boolean.class);
+
+    }
+
 
     private static HttpHeaders getHttpHeaders() {
         String crendential = "rasmooplus:r@sm00";
